134b4a200fee62bb21aafaf0c5802893026e41fc
Hugo Vacher <korrigan33@gmail.com>
2017-09-25 20:22:49 -0400

    Plug form

---
+++ main.js
@@ -0,0 +1,45 @@
+// FORMS
+
+function serializeJson(form) {
+	var data = {};
+	$.each($(form).serializeArray(), function () {
+		data[this.name] = this.value;
+	});
+	return data;
+}
+
+$('#newsletter').validate({
+	submitHandler: function (form) {
+		var $form = $(form);
+		$form.addClass('loading');
+
+		var data = serializeJson(form);
+
+		$form.find('input, button').attr('disabled', true);
+
+		$.ajax({
+			url: $(form).attr('action').replace('/post', '/post-json') + '&c=?',
+			method: 'GET',
+			data: data,
+			dataType: "json",
+			contentType: "application/json; charset=utf-8",
+			success: function (data) {
+				if (data.result !== "success") {
+					alert(data.msg);
+					$form.find('button').attr('disabled', false);
+				} else {
+					form.reset();
+					$form.addClass('sent').removeClass('loading');
+				}
+			},
+			error: function (error) {
+				alert(error);
+				$form.find('button').attr('disabled', false);
+			},
+			complete: function () {
+				$form.addClass('loading');
+			}
+		});
+		return false;
+	}
+});
